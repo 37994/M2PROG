333c976afc207495d3de397562f1f4686b2cb401
Liam <37994@ma-web.nl>
2023-11-21 15:21:27 +0100

    opdracht2

--- 01A/app.js
+++ 01A/app.js
@@ -1,8 +1,21 @@
 class App {
 
     runApplication() {
-
-        console.log('hello World!');
+        let appNaam = 'naam';
+        let versienummer = 9;
+        let versiedatum = new Date();
+        let autheur = 'Liam'
+        let copyright = 'ja'
+        let distributeur = 'iemand'
+        let darkmode = true
+        console.log('App Naam: '+appNaam);
+        console.log('versie nummer: '+versienummer);
+        console.log('versie datum: '+versiedatum);
+        console.log('autheur: '+autheur);
+        console.log('copyrights: '+copyright);
+        console.log('distributeur: '+distributeur);
+        console.log('darkmode: '+darkmode);
+        
     }
 }
 
