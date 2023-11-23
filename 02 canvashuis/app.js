cb38d83504f3cb7bbd45dc23526eba90529a1b22
Liam <37994@ma-web.nl>
2023-11-23 14:46:17 +0100

    kleurtje(canvasopdracht)

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -5,6 +5,9 @@ class App
         console.log("Hello World!");
         let canvas = document.getElementById("canvasId");
         let g = canvas.getContext("2d");
+        g.fillStyle = "blue";
+        g.fillRect(0, 0, canvas.width, canvas.height);
+        g.fillStyle = "grey";
         g.fillRect(0, 0, 10, 10);
         console.log(canvas);
     }
