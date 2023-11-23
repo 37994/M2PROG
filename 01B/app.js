7156a42d2f9b6b167fe35d298972a1d5db2d3245
Liam <37994@ma-web.nl>
2023-11-23 14:36:01 +0100

    canvasOpdracht

---
+++ 01B/app.js
@@ -0,0 +1,23 @@
+class App {
+
+    runApplication() {
+        let appNaam = 'naam';
+        let versienummer = 9;
+        let versiedatum = new Date();
+        let autheur = 'Liam'
+        let copyright = 'ja'
+        let distributeur = 'iemand'
+        let darkmode = true
+        console.log('App Naam: '+appNaam);
+        console.log('versie nummer: '+versienummer);
+        console.log('versie datum: '+versiedatum);
+        console.log('autheur: '+autheur);
+        console.log('copyrights: '+copyright);
+        console.log('distributeur: '+distributeur);
+        console.log('darkmode: '+darkmode);
+        
+    }
+}
+
+let app = new App();
+app.runApplication();
