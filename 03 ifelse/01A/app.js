a863e7b2df40fc5e3e8aacbec4829907fd2f74dc
Liam <37994@ma-web.nl>
2023-11-30 14:16:17 +0100

    opdracht1les3

---
+++ 03 ifelse/01A/app.js
@@ -0,0 +1,12 @@
+class App
+{
+    runApplication()
+    {
+      let title = document.getElementById("newstitle");
+      let gameNewsHead = document.getElementsByClassName('gamenews headline');
+      let gameNews = document.getElementsByClassName('gamenews')
+    }
+}
+
+let app = new App();
+app.runApplication();
