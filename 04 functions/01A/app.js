36ebe313342f0a1f3f222fbf225e3863eb09093c
Liam <37994@ma-web.nl>
2023-12-14 14:49:22 +0100

    les4opdracht4

--- 04 functions/01A/app.js
+++ 04 functions/01A/app.js
@@ -1,29 +1,52 @@
-function argumentsAreHandy(shoutout)
-{
-    console.log('do you want to give a shoutout');
-    console.log(shoutout);
-}
-
-function superMooieGlobalFunction()
-{
-    console.log('ik ben global');
-    console.log('dus je mag mij overal aanroepen');
-}
-
-superMooieGlobalFunction();
-superMooieGlobalFunction();
-superMooieGlobalFunction();
-
-
+//function argumentsAreHandy(shoutout)
+//{
+//    console.log('do you want to give a shoutout');
+//    console.log(shoutout);
+//}
+//
+//function superMooieGlobalFunction()
+//{
+//    console.log('ik ben global');
+//    console.log('dus je mag mij overal aanroepen');
+//}
+//
+//superMooieGlobalFunction();
+//superMooieGlobalFunction();
+//superMooieGlobalFunction();
+//
+//
+//
+//class App {
+//
+//    runApplication() {
+//        console.log('hello world')
+//        superMooieGlobalFunction();
+//
+//    }
+//}
+//
+//let app = new App();
+//app.runApplication();
 
 class App {
 
     runApplication() {
-        console.log('hello world')
-        superMooieGlobalFunction();
 
+        console.log('hello world');
+    }
+
+    newClassFunction() {
+        
+        console.log('hello world in de nieuwclassfunction');
+    }
+
+    anotherFunctionWithArguments(aArgument) {
+        console.log('hier:');
+        console.log(aArgument);
     }
 }
 
 let app = new App();
 app.runApplication();
+app.newClassFunction();
+app.anotherFunctionWithArguments("mijn argument is iets...");
