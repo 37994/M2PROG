41c530f4051bbefcc19f981497040592181d78d6
Liam <37994@ma-web.nl>
2024-01-25 13:16:01 +0100

    les6opdracht3

--- 06 events/app.js
+++ 06 events/app.js
@@ -3,12 +3,41 @@ class App {
     runApplication() {
         console.log("hello world");
         let uiButton = document.getElementById('knop');
+        let mijnButton = document.getElementById('mijnKnop');
+        let Knop3 = document.getElementById('nogEenKnop');
 
         let localeFuntion = function (e) {
             console.log("click!");
         };
         uiButton.addEventListener("click", (e) => {
-            console.log("click!")
+            const para = document.createElement("p");
+            const node = document.createTextNode("this is new");
+            para.appendChild(node);
+            document.body.appendChild(para);
+        });
+
+        mijnButton.addEventListener("click", (e) => {
+            const h1 = document.createElement("h1");
+            const woorden = document.createTextNode("eigen tekst");
+            const img = document.createElement("img");
+
+            img.src = "foto.jpeg";
+
+            h1.appendChild(woorden);
+            document.body.appendChild(h1);
+            document.body.appendChild(img);
+        });
+
+        Knop3.addEventListener("click", (e) => {
+            const h1 = document.createElement("h1");
+            const woorden = document.createTextNode("clicked a div");
+            const img = document.createElement("img");
+
+            img.src = "foto2.jpeg";
+
+            h1.appendChild(woorden);
+            document.body.appendChild(h1);
+            document.body.appendChild(img);
         });
     }
 }
