f08f4b9f9a70e90c353388dcad7d79bb1dba43bc
Liam <37994@ma-web.nl>
2024-01-25 12:34:27 +0100

    les5opdracht6

---
+++ 06 extra/app.js
@@ -0,0 +1,33 @@
+class App {
+    //    mario() {
+    //        console.log('MARIO!!');
+    //    }
+    //
+    //    bwah() {
+    //        console.log('BWAHAHA!!!');
+    //    }
+    //
+    //    maffs() {
+    //        let a = 1;
+    //        let b = 2;
+    //
+    //        let c = (a+b);
+    //        console.log(c);
+    //    }
+    //
+    //    abc() {
+    //        console.log('abc')
+    //    }
+    //    
+    runApplication() {
+        let items = [ ['boom', 'red', 1.2], ['huis', 'yellow', 0.8] ]; 
+        items.push(true, 'muur', 754);
+        items.splice('boom');
+    }
+}
+    
+    let app = new App();
+    app.mario();
+    app.bwah();
+    app.maffs();
+    app.abc();
